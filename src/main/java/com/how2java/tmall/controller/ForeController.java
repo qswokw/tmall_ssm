25223e359bda8016720e99a0b170d84c627129b3
qswokw <qswokw@163.com>
2019-05-06 16:07:04 +0800

    登录界面

--- src/main/java/com/how2java/tmall/controller/ForeController.java
+++ src/main/java/com/how2java/tmall/controller/ForeController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 //前台home页
@@ -56,5 +58,19 @@ public class ForeController {
         }
         userService.add(user);
         return "redirect:registerSuccessPage";
+    }
+
+    //    登录
+    @RequestMapping("forelogin")
+    public String login(@RequestParam("name") String name, @RequestParam("password") String password, Model model, HttpSession session) {
+        name = HtmlUtils.htmlEscape(name);
+        User user = userService.get(name, password);
+        if (user == null) {
+            model.addAttribute("msg", "账户密码错误");
+            return "fore/login";
+
+        }
+        session.setAttribute("user", user);
+        return "redirect:forehome";
     }
 }
--- src/main/java/com/how2java/tmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/UserServiceImpl.java
@@ -39,6 +39,17 @@ public class UserServiceImpl implements UserService {
         example.setOrderByClause("id desc");
         return userMapper.selectByExample(example);
     }
+
+    @Override
+    public User get(String name, String password) {
+        UserExample example = new UserExample();
+        example.createCriteria().andNameEqualTo(name).andPasswordEqualTo(password);
+        List<User> result = userMapper.selectByExample(example);
+        if(result.isEmpty())
+            return null;
+        return result.get(0);
+    }
+
     @Override
     public boolean isExist(String name) {
         UserExample example = new UserExample();
